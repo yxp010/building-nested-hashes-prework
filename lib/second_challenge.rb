2cb0cdc3c7d58a7cc569c143aeebd3c18dbc46dd
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-31 21:30:57 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,19 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {patriarch: {}, matriarch: {}, hero: {}, hero_friends: []},
-    :capulet => {patriarch: {}, matriarch: {}, heroine: {}, heroine_friends: []}
-    }
+    :montague => {
+      #your key/value pairs here!
+      patriarch: {},
+      matriarch: {},
+      hero: {},
+      hero_friends: []
+      },
+    :capulet => {
+      #your key/value pairs here!
+      patriarch: {},
+      matriarch: {},
+      heroine: {},
+      hero_friends: []
+      }}
 
 
 end
